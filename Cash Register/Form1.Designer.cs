06735137c2914aa7a885412c0810255a90b5a0bc
heetpate <heetpate491@ed.amdsb.ca>
2024-02-21 14:45:30 -0500

    Add project files.

---
+++ Cash Register/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Cash_Register
+{
+    partial class cashRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.techShopLabel = new System.Windows.Forms.Label();
+            this.noOfPhone = new System.Windows.Forms.Button();
+            this.noOfLaptop = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.phoneInput = new System.Windows.Forms.TextBox();
+            this.laptopInput = new System.Windows.Forms.TextBox();
+            this.vrInput = new System.Windows.Forms.TextBox();
+            this.caculateButton = new System.Windows.Forms.Button();
+            this.subTotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.receiptPrinter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // techShopLabel
+            // 
+            this.techShopLabel.AutoSize = true;
+            this.techShopLabel.BackColor = System.Drawing.Color.Transparent;
+            this.techShopLabel.Font = new System.Drawing.Font("Stencil", 21.75F);
+            this.techShopLabel.ForeColor = System.Drawing.Color.Cyan;
+            this.techShopLabel.Location = new System.Drawing.Point(299, 9);
+            this.techShopLabel.Name = "techShopLabel";
+            this.techShopLabel.Size = new System.Drawing.Size(169, 34);
+            this.techShopLabel.TabIndex = 0;
+            this.techShopLabel.Text = "Tech Shop";
+            // 
+            // noOfPhone
+            // 
+            this.noOfPhone.BackColor = System.Drawing.Color.DarkBlue;
+            this.noOfPhone.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Bold);
+            this.noOfPhone.ForeColor = System.Drawing.Color.Lime;
+            this.noOfPhone.Location = new System.Drawing.Point(12, 89);
+            this.noOfPhone.Name = "noOfPhone";
+            this.noOfPhone.Size = new System.Drawing.Size(217, 37);
+            this.noOfPhone.TabIndex = 1;
+            this.noOfPhone.Text = "Number of Phone";
+            this.noOfPhone.UseVisualStyleBackColor = false;
+            // 
+            // noOfLaptop
+            // 
+            this.noOfLaptop.BackColor = System.Drawing.Color.DarkBlue;
+            this.noOfLaptop.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Bold);
+            this.noOfLaptop.ForeColor = System.Drawing.Color.Lime;
+            this.noOfLaptop.Location = new System.Drawing.Point(12, 133);
+            this.noOfLaptop.Name = "noOfLaptop";
+            this.noOfLaptop.Size = new System.Drawing.Size(217, 37);
+            this.noOfLaptop.TabIndex = 2;
+            this.noOfLaptop.Text = "Number of Laptop";
+            this.noOfLaptop.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkBlue;
+            this.button2.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(12, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 37);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Number of VR";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // phoneInput
+            // 
+            this.phoneInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.phoneInput.Location = new System.Drawing.Point(251, 97);
+            this.phoneInput.Name = "phoneInput";
+            this.phoneInput.Size = new System.Drawing.Size(62, 21);
+            this.phoneInput.TabIndex = 4;
+            // 
+            // laptopInput
+            // 
+            this.laptopInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.laptopInput.Location = new System.Drawing.Point(251, 141);
+            this.laptopInput.Name = "laptopInput";
+            this.laptopInput.Size = new System.Drawing.Size(62, 21);
+            this.laptopInput.TabIndex = 5;
+            // 
+            // vrInput
+            // 
+            this.vrInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.vrInput.Location = new System.Drawing.Point(251, 184);
+            this.vrInput.Name = "vrInput";
+            this.vrInput.Size = new System.Drawing.Size(62, 21);
+            this.vrInput.TabIndex = 6;
+            // 
+            // caculateButton
+            // 
+            this.caculateButton.BackColor = System.Drawing.Color.Black;
+            this.caculateButton.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.caculateButton.ForeColor = System.Drawing.Color.PaleGreen;
+            this.caculateButton.Location = new System.Drawing.Point(119, 238);
+            this.caculateButton.Name = "caculateButton";
+            this.caculateButton.Size = new System.Drawing.Size(171, 41);
+            this.caculateButton.TabIndex = 7;
+            this.caculateButton.Text = "Calculate";
+            this.caculateButton.UseVisualStyleBackColor = false;
+            this.caculateButton.Click += new System.EventHandler(this.caculateButton_Click);
+            // 
+            // subTotalLabel
+            // 
+            this.subTotalLabel.AutoSize = true;
+            this.subTotalLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTotalLabel.ForeColor = System.Drawing.Color.Lime;
+            this.subTotalLabel.Location = new System.Drawing.Point(389, 85);
+            this.subTotalLabel.Name = "subTotalLabel";
+            this.subTotalLabel.Size = new System.Drawing.Size(87, 19);
+            this.subTotalLabel.TabIndex = 9;
+            this.subTotalLabel.Text = "Subtotal:";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.ForeColor = System.Drawing.Color.Lime;
+            this.taxLabel.Location = new System.Drawing.Point(374, 117);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(112, 19);
+            this.taxLabel.TabIndex = 10;
+            this.taxLabel.Text = "TaxAmount:";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.ForeColor = System.Drawing.Color.Lime;
+            this.totalLabel.Location = new System.Drawing.Point(401, 152);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(59, 19);
+            this.totalLabel.TabIndex = 11;
+            this.totalLabel.Text = "Total:";
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.AutoSize = true;
+            this.tenderedLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedLabel.ForeColor = System.Drawing.Color.Lime;
+            this.tenderedLabel.Location = new System.Drawing.Point(389, 196);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(94, 19);
+            this.tenderedLabel.TabIndex = 12;
+            this.tenderedLabel.Text = "Tendered:";
+            this.tenderedLabel.Click += new System.EventHandler(this.tenderedLabel_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(524, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(536, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(536, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(536, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "...";
+            // 
+            // textInput
+            // 
+            this.textInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.textInput.Location = new System.Drawing.Point(522, 195);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(62, 21);
+            this.textInput.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Stencil", 12.75F);
+            this.button1.ForeColor = System.Drawing.Color.PaleGreen;
+            this.button1.Location = new System.Drawing.Point(366, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 41);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Calculate the Change";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.ForeColor = System.Drawing.Color.Lime;
+            this.changeLabel.Location = new System.Drawing.Point(392, 299);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(79, 19);
+            this.changeLabel.TabIndex = 19;
+            this.changeLabel.Text = "Change:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(545, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "...";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Font = new System.Drawing.Font("Stencil", 12.75F);
+            this.button3.ForeColor = System.Drawing.Color.PaleGreen;
+            this.button3.Location = new System.Drawing.Point(366, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(229, 41);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Print the Receipt";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // receiptPrinter
+            // 
+            this.receiptPrinter.BackColor = System.Drawing.Color.AliceBlue;
+            this.receiptPrinter.Location = new System.Drawing.Point(623, 9);
+            this.receiptPrinter.Name = "receiptPrinter";
+            this.receiptPrinter.Size = new System.Drawing.Size(296, 429);
+            this.receiptPrinter.TabIndex = 22;
+            // 
+            // cashRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkBlue;
+            this.ClientSize = new System.Drawing.Size(940, 450);
+            this.Controls.Add(this.receiptPrinter);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subTotalLabel);
+            this.Controls.Add(this.caculateButton);
+            this.Controls.Add(this.vrInput);
+            this.Controls.Add(this.laptopInput);
+            this.Controls.Add(this.phoneInput);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.noOfLaptop);
+            this.Controls.Add(this.noOfPhone);
+            this.Controls.Add(this.techShopLabel);
+            this.Name = "cashRegister";
+            this.Text = "Tech Shop";
+            this.Load += new System.EventHandler(this.cashRegister_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label techShopLabel;
+        private System.Windows.Forms.Button noOfPhone;
+        private System.Windows.Forms.Button noOfLaptop;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox phoneInput;
+        private System.Windows.Forms.TextBox laptopInput;
+        private System.Windows.Forms.TextBox vrInput;
+        private System.Windows.Forms.Button caculateButton;
+        private System.Windows.Forms.Label subTotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label receiptPrinter;
+    }
+}
+
---
+++ Cash Register/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cash_Register
+{
+    public partial class cashRegister : Form
+    {
+        double phone = 1259.00;
+        double VR = 3200.00;
+        double laptop = 1600.00;
+        public cashRegister()
+        {
+            InitializeComponent();
+        }
+
+        private void cashRegister_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tenderedLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void caculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                phone = Convert.ToInt32(slicesInput.Text);
+
+                subtotal = slicesPrice * numOfSlices;
+                double taxAmount = subtotal * taxRate;
+                double totalPrice = subtotal + taxAmount;
+
+                subtotalOutput.Text = $"{subtotal.ToString("C")}";
+                taxOutput.Text = $"${taxAmount.ToString(".00")}";
+                totalPriceOutput.Text = $"${totalPrice.ToString(".00")}";
+            }
+            catch
+            {
+                subtotalOutput.Text = "ERROR";
+                taxOutput.Text = "ERROR";
+                subtotalOutput.Text = "ERROR";
+                totalPriceOutput.Text = "ERROR";
+
+
+            }
+
+        }
+        }
+    }
+}
